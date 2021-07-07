089d1f3178ce80eeb7a8e59d27d454fa194e2089
Ziv Tamary <zivtamary@gmail.com>
2021-07-07 20:09:16 +0300

    Split functions to chunks

--- utils/savePassword.js
+++ utils/savePassword.js
@@ -5,13 +5,19 @@ const chalk = require('chalk');
 const log = console.log;
 
 const savePassword = (password) => {
-    fs.open(path.join(__dirname, '../', 'passwords.txt'), 'a', 666, (_e, id) => {
-        fs.write(id, password + os.EOL, null, 'utf-8', () => {
-            fs.close(id, () => {
-                log(chalk.green('Password saved to passwords.txt'));
-            });
-        });
-    });
+    fs.open(path.join(__dirname, '../', 'passwords.txt'), 'a', 666, onSave);
+};
+
+const onSave = (_e, id) => {
+    fs.write(id, password + os.EOL, null, 'utf-8', onWrite);
+};
+
+const onWrite = () => {
+    fs.close(id, onFileClose);
+};
+
+const onFileClose = () => {
+    log(chalk.green('Password saved to passwords.txt'));
 };
 
 module.exports = savePassword;
